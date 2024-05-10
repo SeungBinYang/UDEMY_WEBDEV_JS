70a129106c188f65c71a0b39eccb12e97d214d5a
Seungbin <115996269+SeungBinYang@users.noreply.github.com>
2024-05-10 16:21:06 +0900

    new files

---
+++ app.js
@@ -0,0 +1,7 @@
+let age = 22;
+let userName = 'Seungbin';
+let hobbies = ['Gaming', 'Youtube', 'Reading'];
+let job = { title: 'Developer', place: 'New York', salary: 50000 };
+
+
+alert(hobbies[1]);
